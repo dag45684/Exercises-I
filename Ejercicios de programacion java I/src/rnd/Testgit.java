429049f7d5f367357edfdb6fc7799621705f858d
Carlos DAM <dag45684@educastur.es>
2022-10-26 16:28:06 +0200

    test

---
+++ Ejercicios de programacion java I/src/rnd/Testgit.java
@@ -0,0 +1,12 @@
+package rnd;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+
+	}
+
+}
--- Ejercicios de programacion java I/src/rnd/test.java
+++ Ejercicios de programacion java I/src/rnd/test.java
@@ -5,7 +5,9 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		
+		String s1 = "Hello world!";
+		String s2 = "Hello world!";
+		System.out.println(s1==s2);
 		
 	}
 
