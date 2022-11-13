ade77a2e6e533473b7b7d8b65934060af90fbf7d
Carlos <carlosmgov7@gmail.com>
2022-11-13 11:18:15 +0100

    home work

--- Ejercicios de programacion java I/src/part3/Exercise20p3.java
+++ Ejercicios de programacion java I/src/part3/Exercise20p3.java
@@ -11,15 +11,18 @@ import java.util.Scanner;
 public class Exercise20p3 {
 
 	static int [][] ex;
-	
+
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		boolean end = false;
 		while (!end) {
 			System.out.print("Insert Command\n>");
 			instruction(br.readLine());
 		} 
+	}
+
+	static void print(int[][] ex) {
+		for ( int[] x : ex) System.out.println(Arrays.toString(x));
 	}
 
 	static boolean instruction (String instruction){
@@ -35,7 +38,8 @@ public class Exercise20p3 {
 				swap(scanner);
 				break;
 			case "print":
-				System.out.println(Arrays.toString(ex));
+				print(ex);
+				break;
 			default:
 				scanner.close();
 				return true;
@@ -61,7 +65,7 @@ public class Exercise20p3 {
 		}catch (NoSuchElementException | IllegalStateException | NumberFormatException e) {
 			System.out.println("Wrong Command. Pattern is >array NxM: n1 n2 n3...");
 		}
-		System.out.println(Arrays.toString(ex));
+		print(ex);
 	}
 	
 	static void swap (Scanner scanner) {
@@ -75,23 +79,9 @@ public class Exercise20p3 {
 			ex[s][ss] = temp;
 		}catch (NoSuchElementException e) {
 			System.out.println("Wrong command. Partter is >swap pos1.1 pos1.2 pos2.1 pos2.2");
+		}catch (IndexOutOfBoundsException e) {
+			System.out.println("Wrong index.");
 		}
-		System.out.println(Arrays.toString(ex));
+		print(ex);
 	}
-		
-	
-//	static void array (String instruction) {
-//		
-//		try {
-//			int x = instruction.indexOf("x");
-//			int semi = instruction.indexOf(":");
-//			int n = Integer.valueOf(instruction.substring(0,x));
-//			int m = Integer.valueOf(instruction.substring(x));
-//			
-//		}catch (NumberFormatException e) {
-//			System.out.println("Wrong command. Format is:\nNxM: n1 n2 n3...");
-//		}
-//		
-//	}
-	
 }
