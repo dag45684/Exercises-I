e19fc16bc17d09563504b133736059fb864ae284
Carlos DAM <dag45684@educastur.es>
2022-11-23 17:28:54 +0100

    idk

--- Ejercicios de programacion java I/src/rnd/test.java
+++ Ejercicios de programacion java I/src/rnd/test.java
@@ -4,15 +4,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class test {
-	
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
-		System.out.println("Test");
-		
-		
+		int [] n = {1,2,3,4,5,6,7,8,9};
+		for (int i=0; i<n.length; i++) {
+			System.out.println(operation(n[i]));
+		}
+	}
+	
+	public static int operation (int n) {
+		return ((n+2)-1)*3;
 	}
 
 }
