a81e48c40d25ec2376e87948b567efe509eb7792
Carlos DAM <dag45684@educastur.es>
2023-01-18 17:34:19 +0100

    Enum lesson

--- Ejercicios de programacion java I/src/enums/Hand.java
+++ Ejercicios de programacion java I/src/enums/Hand.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class Hand {
 
-	ArrayList<Card> content = new ArrayList<Card>();
+	private ArrayList<Card> content = new ArrayList<Card>();
 	
 	public Hand () {
 		for (Suites suite : Suites.values()) {
 			for (Numbers number : Numbers.values()) {
-				content.add(new Card(suite, number));
+				this.content.add(new Card(suite, number));
 			}
 		}
 	}
