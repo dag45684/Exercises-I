fe3afb047c40ee83bcd676a9e8705cb6d52e0630
Carlos DAM <dag45684@educastur.es>
2023-01-23 18:39:14 +0100

    dont mind this

--- Ejercicios de programacion java I/src/rnd/Day7.java
+++ Ejercicios de programacion java I/src/rnd/Day7.java
@@ -15,14 +15,37 @@ public class Day7 {
 		while (scanner.hasNextLine()) data.add(scanner.nextLine());
 		// Parsing
 		
-		
-		
-		
-		
-		
-		
-		
-
+		Node root = new Node("/");
+		Node current = root;
+		current.setIsDir();
+		for(int i=0; i<data.size(); i++) {
+			if (data.get(i).contains("$")){
+				switch (data.get(i).substring(1, 4)) {
+				case "cd":
+					if (data.get(i).contains("..")){
+						current = current.getParent();
+					}
+					else { 
+						current.setIsDir();
+						Node creation = new Node(data.get(i).substring(5),current);
+						current.getContent().add(creation);
+						current = creation;
+						current.setIsDir();
+						break;
+					}
+				case "ls": break;
+				}
+			}
+			else {
+				if (!data.get(i).contains("dir")) {
+					int blank = data.get(i).indexOf(" ");
+					int size = Integer.parseInt(data.get(i).substring(0, blank));
+					current.getContent().add(new Node(data.get(i).substring(blank+1), size));
+				}
+			}
+		}
+		Node.dirSizeCalc(root);
+		System.out.println(root.toString());
 	}
 
 }
--- Ejercicios de programacion java I/src/rnd/Node.java
+++ Ejercicios de programacion java I/src/rnd/Node.java
@@ -12,20 +12,35 @@ public class Node {
 	private boolean hasDir = false;
 	private int size;
 	
+	public Node (String name) {
+		this.name = name;
+	}
 	public Node (String name, Node parent) {
 		this.name = name;
 		this.parent = parent;
+	}
+	public Node (String name, int size) {
+		this.name = name;
+		this.size = size;
 	}
 
 	public void setName (String name) {this.name = name;}
 
 	public void setParent (Node parent) {this.parent = parent;}
 
+	public void setSize (int size) {this.size = size;}
+
 	public void setIsDir () {this.isDir = true;}
 
 	public void setHasDir () {this.hasDir = true;}
 
+	public boolean getIsDir () {return this.isDir;}
+
+	public boolean getHasDir () {return this.hasDir;}
+
 	public String getName() {return this.name;}
+
+	public int getSize() {return this.size;}
 
 	public ArrayList<Node> getContent() {return this.content;}
 
@@ -34,12 +49,27 @@ public class Node {
 	@Override
 	public String toString () {
 		String res = this.name + "  " + this.size + "Kb \n";
-		if (this.hasDir) {
-			for (int i=0; i<this.content.size(); i++) {
-				res += this.content.get(i).getName() + '\n';
+		for (int i=0; i<this.content.size(); i++) {
+			if(this.content.get(i).isDir) {
+				res += this.content.get(i).toString();
+			}
+			else res += "-----" + this.content.get(i).getName() + "  " + this.content.get(i).getSize() + "Kb \n";
+		}
+		return res;
+	}
+	
+	public static int dirSizeCalc(Node node) {
+		int res = 0;
+		for (int i=0; i<node.getContent().size(); i++) {
+			if (node.getContent().get(i).getIsDir()) {
+				dirSizeCalc(node);
+			}
+			else {
+				res += node.getSize();
 			}
 		}
-		return "a";
+		node.setSize(res);
+		return node.getSize();
 	}
 	
 	
