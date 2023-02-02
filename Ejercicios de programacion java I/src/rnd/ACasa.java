e5818ada21fdfaa9e0948f2ff6e34a6c8e3d3dde
Carlos DAM <dag45684@educastur.es>
2023-02-02 20:06:16 +0100

    oops fix

--- Ejercicios de programacion java I/src/rnd/ACasa.java
+++ Ejercicios de programacion java I/src/rnd/ACasa.java
@@ -4,7 +4,7 @@ public class ACasa {
 
 	public static void main(String[] args) {
 		
-		int h = (int) (Math.random() * 2);
+		int h = (int) (Math.random() * 1);
 		int m = (int) (Math.random() * 59);
 		System.out.println("Nos vamos pa casa dentro de " + h + " horas y " + m + " minutos." );
 
