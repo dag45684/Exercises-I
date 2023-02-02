65f1ea13859a44b0782aff2513c1c1c6f1132cf2
Carlos DAM <dag45684@educastur.es>
2023-02-02 20:05:44 +0100

    impressive feature

---
+++ Ejercicios de programacion java I/src/rnd/ACasa.java
@@ -0,0 +1,13 @@
+package rnd;
+
+public class ACasa {
+
+	public static void main(String[] args) {
+		
+		int h = (int) (Math.random() * 2);
+		int m = (int) (Math.random() * 59);
+		System.out.println("Nos vamos pa casa dentro de " + h + " horas y " + m + " minutos." );
+
+	}
+
+}
