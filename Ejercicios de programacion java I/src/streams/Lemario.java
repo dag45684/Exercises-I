2f71c4f5042bcbfbfd6a445cf5ad0a5daa128ddd
Carlos DAM <dag45684@educastur.es>
2023-03-01 17:40:02 +0100

    Streams excs

--- Ejercicios de programacion java I/src/streams/Lemario.java
+++ Ejercicios de programacion java I/src/streams/Lemario.java
@@ -3,7 +3,11 @@ package streams;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Lemario {
 
@@ -17,6 +21,36 @@ public class Lemario {
 		//1.Show in console all words starting by letters
 		// 	from a to m.
 
+//		lemario.stream().filter(n -> n.matches("[a-m].*")).forEach(System.out::println);
+		
+		//2.Group words that begin with the same three characters when length over 3
+		
+		Map<String, ArrayList<String>> gr = new HashMap<>();
+//		lemario.forEach(n -> {
+//			if (n.length() > 3 && !gr.containsKey(n.substring(0, 3))) {
+//				gr.put(n.substring(0, 3), new ArrayList<>());
+//			}			
+//			gr.get(n.substring(0, 3)).add(n);
+//			
+//		});
+//		gr.forEach((k, v) -> System.out.println(k + " " + v));
+		
+//		Map <String, List<String>> res;
+//		res = lemario.stream()
+//				.filter(e -> e.length()>3)
+//				.collect(Collectors.groupingBy(e -> e.substring(0, 3)));
+//		res.forEach((k, v) -> System.out.println(k + " " + v));
+		
+		//3. Find palyndromes and store them in a list
+		List<String> pal;
+		pal = lemario.stream().filter(n -> n.equals(new StringBuilder(n).reverse().toString())).toList();
+		pal.forEach(System.out::println);
+		
+		//4. count the number of words that each letter appears in
+		
+		
+//		lemario.stream().
+		
 	}
 
 }
