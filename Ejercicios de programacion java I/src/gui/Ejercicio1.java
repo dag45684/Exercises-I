48b44954b41367326e8471c08e405339513cc0c6
Carlos DAM <dag45684@educastur.es>
2023-04-13 21:26:29 +0200

    Comment on pack function

--- Ejercicios de programacion java I/src/gui/Ejercicio1.java
+++ Ejercicios de programacion java I/src/gui/Ejercicio1.java
@@ -10,25 +10,16 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
- public class App2 extends JFrame implements ActionListener{
-	 
+public class Ejercicio1 extends JFrame implements ActionListener {
+
 	 private static final long serialVersionUID = 1L;
 	 
-	 App2(String[] args){
+	 Ejercicio1(String[] args){
 		 super("My app");
 		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 
 		 Container contentPane = getContentPane();
-		 JButton button = new JButton("click here");
-		 JTextField text = new JTextField();
-		 button.addActionListener(e -> {
-			System.out.println("Hello World!"); 
-		 	text.setText(text.getText().isEmpty() ? "Yay! :D" : "");
-		 });
-		 //this lambda substitutes the override on the action performed
-		 contentPane.add(button, BorderLayout.NORTH);
-		 contentPane.add(text, BorderLayout.CENTER);
-		 
+
 		 pack(); //Dimensiona la ventana en base al contenido que le hayamos agregado
 		 setLocationRelativeTo(null);
 	 }
@@ -37,10 +28,11 @@ import javax.swing.SwingUtilities;
 		 SwingUtilities.invokeLater(() -> new App2(args).setVisible(true));
 		 
 	 }
-	 
-	 @Override
-	 public void actionPerformed(ActionEvent e) {
-		 System.out.println("Hello world!");
-	 }
- 
- }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
